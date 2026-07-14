d8b2992e343e03a1f268096c34a7c4640bf2c93a
dcus official - account <tsukgaka759@gmail.com>
2026-07-15 08:49:07 +0900

    Create list.lua

---
+++ games/list.lua
@@ -0,0 +1,8 @@
+return{
+	[11815767793] = {
+		Script = "https://api.jnkie.com/api/v1/luascripts/public/64c02c9b46a1a9632b3f264153b385780d519b6720cdba7bf79b5ab0ce4a098b/download",
+	},
+	[6961824067] = {
+		Script = "https://api.jnkie.com/api/v1/luascripts/public/e01192a1262cfdd0cce385c547d311afb423b9ecabab202be9492d4a3628e982/download",
+	}
+}
